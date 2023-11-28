5f3c1886c3d73a405ec5f6a7b47c80ba7c01242b
Celil Ozturk <celil.ozturk@windowslive.com>
2023-11-28 11:49:31 +0300

    TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback components

--- App.tsx
+++ App.tsx
@@ -8,26 +8,44 @@
 import React from 'react';
 import type { PropsWithChildren } from 'react';
 import {
+  Alert,
   Image,
   SafeAreaView,
   StyleSheet,
   Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 
 
 function App(): JSX.Element {
+  const onpress = () => Alert.alert('tiklandi');
   return (
     <SafeAreaView style={styles.container}>
       <View>
+        <TouchableOpacity onPress={onpress}>
+          <Image
+            source={{ uri: 'https://picsum.photos/200/300' }}
+            style={{ width: 100, height: 150 }}
+            blurRadius={10}
+            fadeDuration={100}
+          />
+        </TouchableOpacity>
+        <TouchableWithoutFeedback onPress={()=>console.log("2.resme tiklandi")} >
         <Image
-        source={{ uri: 'https://picsum.photos/200/300'}}
-        style={{ width: 200, height: 300 }}
-        blurRadius={10}
-        fadeDuration={100}
-        />
+            source={{ uri: 'https://picsum.photos/200/300' }}
+            style={{ width: 100, height: 150 }}
+            blurRadius={10}
+            fadeDuration={100}
+          />
+        </TouchableWithoutFeedback>
         <Text>Image from url</Text>
-        <Image source={require('./images/tree.jpg')} style={{ width: 200, height: 300 }} blurRadius={2}  />
+        <TouchableHighlight activeOpacity={0.3}
+          underlayColor="red" onPress={onpress} >
+          <Image source={require('./images/tree.jpg')} style={{ width: 50, height: 50 }} blurRadius={2} />
+        </TouchableHighlight>
         <Text>static image</Text>
       </View>
     </SafeAreaView>
@@ -39,8 +57,8 @@ const styles = StyleSheet.create({
     marginTop: 20,
     color: '#083061',
     backgroundColor: '#2596be',
-    alignItems:'center',
-    justifyContent:'center'
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 });
 
