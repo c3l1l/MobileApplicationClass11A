63f61bf27a5b108ce38d44c446fe0df14784f07a
Celil Ozturk <celil.ozturk@windowslive.com>
2023-12-06 10:21:42 +0300

    TextInput and properties

--- App.tsx
+++ App.tsx
@@ -6,60 +6,93 @@
  */
 
 import React from 'react';
-import type { PropsWithChildren } from 'react';
 import {
-  Alert,
-  Image,
-  SafeAreaView,
   StyleSheet,
   Text,
-  TouchableHighlight,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
+  Button,
+  TextInput,
   View,
 } from 'react-native';
 
 
 function App(): JSX.Element {
-  const onpress = () => Alert.alert('tiklandi');
+  const [text, onChangeText] = React.useState();
+  const [sifre, onChangeSifre] = React.useState();
+  console.log(text);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <TouchableOpacity onPress={onpress}>
-          <Image
-            source={{ uri: 'https://picsum.photos/200/300' }}
-            style={{ width: 100, height: 150 }}
-            blurRadius={10}
-            fadeDuration={100}
-          />
-        </TouchableOpacity>
-        <TouchableWithoutFeedback onPress={()=>console.log("2.resme tiklandi")} >
-        <Image
-            source={{ uri: 'https://picsum.photos/200/300' }}
-            style={{ width: 100, height: 150 }}
-            blurRadius={10}
-            fadeDuration={100}
-          />
-        </TouchableWithoutFeedback>
-        <Text>Image from url</Text>
-        <TouchableHighlight activeOpacity={0.3}
-          underlayColor="red" onPress={onpress} >
-          <Image source={require('./images/tree.jpg')} style={{ width: 50, height: 50 }} blurRadius={2} />
-        </TouchableHighlight>
-        <Text>static image</Text>
-      </View>
-    </SafeAreaView>
+    <View style={{ flex: 1 }}>
+    <View style={styles.header}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Kullanıcı Adı Giriniz..."
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeSifre}
+        value={sifre}
+        keyboardType="visible-password"
+        placeholder="Şifre Giriniz..."
+        secureTextEntry={true}
+      />
+    </View>
+    <View style={styles.body}>
+      <Button
+        title="Giriş Yap"
+        onPress={() => console.log(text + " " + sifre)}
+      />
+    </View>
+    <View style={styles.footer}>
+      <Text>Made by Ali</Text>
+    </View>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 20,
-    color: '#083061',
-    backgroundColor: '#2596be',
-    alignItems: 'center',
-    justifyContent: 'center'
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
   },
+  header: {
+    flex: 0.6,
+    backgroundColor: "#f5f5dc",
+    borderWidth: 3,
+    borderColor: "black",
+    borderBottomEndRadius: 15,
+    borderBottomStartRadius: 15,
+    padding: 10,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  body: {
+    flex: 0.2,
+    backgroundColor: "azure",
+    borderWidth: 3,
+    borderColor: "black",
+    borderRadius: 15,
+    padding: 10,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  footer: {
+    flex: 0.2,
+    backgroundColor: "#FAEBD7",
+    borderWidth: 3,
+    borderColor: "black",
+    borderTopStartRadius: 15,
+    borderTopEndRadius: 15,
+    padding: 10,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default App;
