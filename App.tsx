8b4cffc0419238dd175ec67ece70df7e5154ff34
Celil Ozturk <celil.ozturk@windowslive.com>
2023-12-12 10:23:41 +0300

    function2 factorial

--- App.tsx
+++ App.tsx
@@ -1,30 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {StyleSheet,View,Text} from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
 
-const Faktoriyel=(value: number)=>{
-  let sonuc=1;
-  for(let i=1;i<=value;i++){
-    sonuc=sonuc*1
-;  }
-
+let sayi= Math.floor(Math.random() * 10);
+const Faktoriyel = (value: number) => {
+  let sonuc = 1;
+  for (let i = 1; i <= value; i++) {
+    sonuc = sonuc * i;
+  }
+console.log(sonuc);
   return sonuc;
- }
- let sayi=Math.floor(Math.random()*10);
+}
 
 function App(): JSX.Element {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>{sayi}!={Faktoriyel(sayi)}</Text>
-  </View>
+    <SafeAreaView style={stiller.container}>
+      <TouchableOpacity style={stiller.box}>
+        <Text style={stiller.yaziStil}>{sayi}!={Faktoriyel(sayi)}</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
+
+const stiller=StyleSheet.create({
+container:{
+   flex: 1,
+   justifyContent:'center',
+   alignItems:'center',
+   backgroundColor:'#ecf0f1',
+   padding:0,
+},
+box:{
+  height:45,
+  width:250,
+  borderColor:'black',
+  borderRadius:20,
+  borderWidth:3,
+  backgroundColor:'bisque',
+  justifyContent:'center',
+  alignItems:'center'
+},
+yaziStil:{
+  fontSize:20,
+  color:'orange'
+}
+});
 
 
 export default App;
